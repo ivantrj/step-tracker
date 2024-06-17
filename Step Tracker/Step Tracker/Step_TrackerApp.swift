1822c44b0af064c2b31dadccbcdef74ca432c545
Ivan Trajanovski <ivantrajanovski@gmail.com>
2024-06-17 10:58:35 +0200

    create xcode project

---
+++ Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Ivan Trajanovski  on 17.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
